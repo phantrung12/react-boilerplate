faf41297d6357c2c68fcf5110111e00a5b699b91
trungtp <tptrung1292@gmail.com>
2025-02-14 14:03:26 +0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
-import { UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { Layout, Menu } from 'antd';
-import { Outlet } from 'react-router';
+import { MenuFoldOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import React from 'react';
 
-const { Header, Content } = Layout;
+const { Header, Content, Sider } = Layout;
+
+const siderStyle: React.CSSProperties = {
+  overflow: 'auto',
+  height: '100vh',
+  position: 'sticky',
+  insetInlineStart: 0,
+  top: 0,
+  bottom: 0,
+  // scrollbarWidth: 'thin',
+  // scrollbarGutter: 'stable',
+};
 
 const Home = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
   const menuItems = [
     {
       key: '1',
@@ -19,35 +33,44 @@ const Home = () => {
   ];
 
   return (
-    <Layout>
-      <Header
-        style={{
-          position: 'sticky',
-          top: 0,
-          zIndex: 1,
-          width: '100%',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <div className="demo-logo" style={{ minWidth: 120, background: '#fff', minHeight: 48 }} />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          items={menuItems}
-          style={{ flex: 1, minWidth: 0 }}
-        />
-      </Header>
-      <Layout style={{ minHeight: '100vh', padding: '0 48px' }}>
-        <Content
+    <Layout hasSider>
+      <Sider style={siderStyle}>
+        <div className="demo-logo-vertical" />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={menuItems} />
+      </Sider>
+      <Layout>
+        <Header
           style={{
-            padding: 24,
-            minHeight: 280,
+            background: colorBgContainer,
+            position: 'sticky',
+            padding: '0px 12px',
+            top: 0,
+            zIndex: 1,
+            boxShadow: '0 2px 8px rgba(0, 0, 0, 0.15)',
           }}
         >
-          HomePage
-          <Outlet />
+          <Button icon={<MenuFoldOutlined />} type="text"></Button>
+        </Header>
+        <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+          <div
+            style={{
+              padding: 24,
+              textAlign: 'center',
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+            }}
+          >
+            <p>long content</p>
+            {
+              // indicates very long content
+              Array.from({ length: 100 }, (_, index) => (
+                <React.Fragment key={index}>
+                  {index % 20 === 0 && index ? 'more' : '...'}
+                  <br />
+                </React.Fragment>
+              ))
+            }
+          </div>
         </Content>
       </Layout>
     </Layout>
